417eaa9846e8890987c5ca5907a1f39920b8c845
BennX <bme10@tu-clausthal>
2015-09-29 14:56:14 +0200

    deleted endcond

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 ############################################################################
 # GPL License                                                              #
 #                                                                          #
-# This file is part of the MASeRaTi.                                       #
-# Copyright (c) 2012-14, Philipp Kraus, <philipp.kraus@tu-clausthal.de>    #
+# This file is part of the JIM-DB.                                         #
+# Copyright (c) 2015, Benjamin Meyer, <benjamin.meyer@tu-clausthal.de>     #
 # This program is free software: you can redistribute it and/or modify     #
 # it under the terms of the GNU General Public License as                  #
 # published by the Free Software Foundation, either version 3 of the       #
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License        #
 # along with this program. If not, see <http://www.gnu.org/licenses/>.     #
 ############################################################################
-@endcond
 **/
 
 /** \mainpage Wellcome to JIM-DB
