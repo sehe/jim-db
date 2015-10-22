0b81d4a25fe3e528a04fa26b42756e0f81be49fa
BennX <bennimeyer@web.de>
2015-10-22 14:25:13 +0200

    added missing docu

--- src/common/fnvhash.h
+++ src/common/fnvhash.h
@@ -21,18 +21,40 @@
 #pragma once
 #include <string>
 
-namespace jimdb {
-	namespace common {
-		class FNVHash
-		{
-		public:
-			inline size_t operator()(const std::string& string);
-			inline size_t operator()(const char* c);
-		private:
-			/* magic 64bit numbers from http://www.isthe.com/chongo/tech/comp/fnv/ */
-			static const size_t m_seed = 0xcbf29ce484222325ULL; // 14695981039346656037
-			static const size_t m_prime = 0x100000001b3ULL;// 1099511628211
-		};
+namespace jimdb
+{
+    namespace common
+    {
+
+        /**
+        \brief Simple hash class
+
+        see http://create.stephan-brumme.com/fnv-hash/ for the hashfunction.
+        It generates a 64bit Hash value.
+
+        It is slower then the std::hash BUT it takes all chars into account while,
+        the std::hash meight not take all. It can takes a subset of them depending on the
+        implementation which is not in the standart!
+        http://stackoverflow.com/questions/7968674/unexpected-collision-with-stdhash
+        @author Benjamin Meyer
+        @date 11.10.2015 17:31
+        */
+        class FNVHash
+        {
+        public:
+            /**
+            \brief hash a c++ string
+            @return a 64bit hashvalue
+            @author Benjamin Meyer
+            @date 11.10.2015 17:48
+            */
+            inline size_t operator()(const std::string& string);
+            inline size_t operator()(const char* c);
+        private:
+            /* magic 64bit numbers from http://www.isthe.com/chongo/tech/comp/fnv/ */
+            static const size_t m_seed = 0xcbf29ce484222325ULL; // 14695981039346656037
+            static const size_t m_prime = 0x100000001b3ULL;// 1099511628211
+        };
 #include "fnvhash.hpp"
-	}
+    }
 }
